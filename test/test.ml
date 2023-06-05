b16cf6df7c6a50bb4336fd82a883f1c4d27aa1e3
Mylloon <kennel.anri@tutanota.com>
2023-06-05 20:46:02 +0200

    add a test

--- test/test.ml
+++ test/test.ml
@@ -10,9 +10,12 @@ let () =
     ; num_additionals = 0
     }
   in
+  (* Header *)
   assert (
     "\\x13\\x14\\x00\\x00\\x00\\x01\\x00\\x00\\x00\\x00\\x00\\x00"
     = Utils.get_bytecode (Query.header_to_bytes data));
-  assert (
-    "\\x06google\\x03com\\x00" = Utils.get_bytecode (Query.encode_dns_name "google.com"))
+  (* Dns name *)
+  let dns_name = Query.encode_dns_name "google.com" in
+  assert ("\\x06google\\x03com\\x00" = Utils.get_bytecode dns_name);
+  assert (6 = Char.code (Bytes.get dns_name 0))
 ;;
