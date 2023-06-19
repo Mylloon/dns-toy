231868e46d9d37068ff2c304721948c9bd2d9425
Mylloon <kennel.anri@tutanota.com>
2023-06-19 22:52:45 +0200

    keep the test with example.com

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,7 @@
 open Dnstoy
 
 let () =
-  let response = Network.send_request "8.8.8.8" "www.mylloon.fr" in
+  let response = Network.send_request "8.8.8.8" "www.example.com" in
   print_endline (Utils.get_bytecode response);
   let dns_packet = Response.parse_dns_packet response in
   print_endline (Debug.dns_packet dns_packet)
