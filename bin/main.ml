48589ff6e03adb1c5af3235dacff202fc8bbf6d4
Mylloon <kennel.anri@tutanota.com>
2023-07-04 10:50:03 +0200

    update debugging tests

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,19 @@
 open Dnstoy
 
+let print = function
+  | Some value -> print_endline (Utils.get_bytecode value)
+  | None -> print_endline "No IP found"
+;;
+
 let () =
-  let response = Network.send_request "8.8.8.8" "www.mylloon.fr" in
+  let response = Network.send_request "8.8.8.8" "www.mylloon.fr" Types.DNSType.a in
   print_endline (Utils.get_bytecode response);
   let dns_packet = Response.parse_dns_packet response in
   print_endline (Debug.dns_packet dns_packet);
-  print_endline (Network.lookup_domain "example.com");
-  print_endline (Network.lookup_domain "recurse.com");
-  print_endline (Network.lookup_domain "www.metafilter.com")
+  print (Network.lookup_domain "example.com");
+  print (Network.lookup_domain "recurse.com");
+  print (Network.lookup_domain "www.metafilter.com");
+  List.iter
+    (fun e -> print_endline (Debug.dns_record e))
+    (Network.send "8.8.8.8" "example.com" 16).answers
 ;;
